58d74b1dd4bea76a01edc852c358772ab74a9706
bluesky7660 <chle7660@gmail.com>
2024-10-15 14:47:32 +0900

    base

---
+++ lala/src/main/java/com/lalaping/LalaApplication.java
@@ -0,0 +1,13 @@
+package com.lalaping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LalaApplication.class, args);
+	}
+
+}
