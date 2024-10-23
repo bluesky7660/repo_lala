77b52f220e5e49e032108701c4d12cd0d7ef578a
bluesky7660 <chlee7660@gmail.com>
2024-10-23 19:51:49 +0900

    1023

--- lala/src/main/java/com/lalaping/dashboard/DashboardController.java
+++ lala/src/main/java/com/lalaping/dashboard/DashboardController.java
@@ -6,6 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.lalaping.complaint.ComplaintService;
+import com.lalaping.complaint.ReceptionVo;
+import com.lalaping.qna.QnaRequestVo;
+import com.lalaping.qna.QnaService;
 
 
 @Controller
@@ -13,9 +16,16 @@ public class DashboardController {
 	@Autowired
 	ComplaintService complaintService;
 	
+	@Autowired
+	QnaService qnaService;
+	
 	@RequestMapping(value = "/v1/dashboard")
-	public String index(Model model) {
-		System.out.println("getAwDept:"+complaintService.answerTypeCount().get(0).getCdSeq());
+	public String index(Model model, QnaRequestVo qnaRequestVo, ReceptionVo receptionVo) {
+//		System.out.println("getAwDept:"+complaintService.answerTypeCount().get(0).getCdSeq());
+		qnaRequestVo.setRowNumToShow(5);
+		receptionVo.setRowNumToShow(5);
+		model.addAttribute("qnaList", qnaService.selectListRequest(qnaRequestVo));
+		model.addAttribute("comList", complaintService.receptionSelectList(receptionVo));
 		model.addAttribute("DeptType", complaintService.answerTypeCount());
 		model.addAttribute("CompType", complaintService.receptionTypeCount());
 		return "xdm/v1/index";
